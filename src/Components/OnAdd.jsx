e2e93763adb1b01642d7bcb4fc1ee5c9f12f22d6
xxpalinxx <hkpalin@gmail.com>
2024-09-20 17:40:12 -0300

    logica para no restar productos por debejo de cero

--- src/Components/OnAdd.jsx
+++ src/Components/OnAdd.jsx
@@ -4,13 +4,35 @@ import "../styles.css"
 const OnAdd = () => {
     const [count,setCount] = useState(0)
 
+    /* const handleCount = (contador) => {
+        if (contador === 0) {
+            return (<div className='contador'>
+                <button className='btnSumarRestar'> - </button>
+                <h3>{count}</h3>
+                <button className='btnSumarRestar' onClick={()=>setCount(count => count+1)}> + </button>
+            </div>)
+        }
+        return (<div className='contador'>
+                <button className='btnSumarRestar' onClick={()=>setCount(count => count-1)}> - </button>
+                <h3>{count}</h3>
+                <button className='btnSumarRestar' onClick={()=>setCount(count => count+1)}> + </button>
+            </div> )
+    } */
+
     return (
         <div className='card'>
+            {count > 0 ?
             <div className='contador'>
                 <button className='btnSumarRestar' onClick={()=>setCount(count => count-1)}> - </button>
                 <h3>{count}</h3>
                 <button className='btnSumarRestar' onClick={()=>setCount(count => count+1)}> + </button>
-            </div>
+            </div> :
+            <div className='contador'>
+                <button className='btnSumarRestar'> - </button>
+                <h3>{count}</h3>
+                <button className='btnSumarRestar' onClick={()=>setCount(count => count+1)}> + </button>
+            </div>}
+            
         </div>
     )
 }
